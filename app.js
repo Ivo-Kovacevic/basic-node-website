801f1a10fe6f33e3746f2fead4617d9d915087ea
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-08-12 20:45:59 +0200

    Rewrite app with Express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'about.html')));
+app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, 'contact-me.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '404.html')));
+
+app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`));
